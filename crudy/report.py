d4b69f92d5185e1ebc194f60afa54121fe28d625
Jose Cupertino Ruiz Vargas <jruizvar@ncc.unesp.br>
2018-06-18 18:49:15 -0300

    Read SQL query with pandas

--- crudy/report.py
+++ crudy/report.py
@@ -10,18 +10,11 @@ bp = Blueprint('report', __name__, url_prefix='/report')
 
 @bp.route('/')
 def read():
-    db = get_db().cursor()
-    query = db.execute(
-        'SELECT m.order_id, p.name, p.price, o.created '
-        'FROM middle AS m, products AS p, orders AS o '
-        'WHERE m.prod_id = p.id AND m.order_id = o.id'
-    )
-    cols = [key[0] for key in db.description]
-    results = [dict(zip(cols, row)) for row in query]
-    dd = defaultdict(list)
-    for d in results:
-        for k, v in d.items():
-            dd[k].append(v)
-    df = pd.DataFrame.from_dict(dd)
+    db = get_db()
+    query = """ SELECT m.order_id, p.name, p.price, o.created
+                FROM middle AS m, products AS p, orders AS o 
+                WHERE m.prod_id = p.id AND m.order_id = o.id
+            """
+    df = pd.read_sql(query, db)
     df = df.to_html(index=False, justify='center')
     return render_template('report/view.html', df=df)
